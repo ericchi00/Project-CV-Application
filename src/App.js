6eb22665e5cb259b34f3a5714b5d75cde60d1be9
ericchi00 <ericchi00@gmail.com>
2022-04-06 17:48:18 -0700

    add work button works

--- src/App.js
+++ src/App.js
@@ -1,20 +1,77 @@
 import React from 'react';
-import Education from './components/education';
-import Personal from './components/personal';
-import Work from './components/work';
+import Education from './components/Education';
+import Personal from './components/Personal';
+import Work from './components/Work';
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = '';
+		this.state = {
+			work: [],
+			workCount: 0,
+			education: [],
+			educationCount: 0,
+		};
+		this.addWork = this.addWork.bind(this);
 	}
+
+	addWork() {
+		this.setState({
+			workCount: this.state.workCount + 1,
+			work: this.state.work.concat(this.state.workCount),
+		});
+	}
+
+	addEducation() {
+		this.setState({
+			educationCount: this.state.educationCount + 1,
+			education: this.state.education.concat(this.state.educationCount),
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<form>
-					<Personal />
-					<Work />
-					<Education />
+					<fieldset>
+						<Personal />
+					</fieldset>
+					<fieldset>
+						<ul>
+							<li>
+								<legend>Work Experience</legend>
+								<label htmlFor="company"></label>
+								<input type="text" id="company" placeholder="Company" />
+								<label htmlFor="companyTitle"></label>
+								<input type="text" id="companyTitle" placeholder="Title" />
+								<label htmlFor="workStart"></label>
+								<input type="text" id="workStart" placeholder="Start Date" />
+								<label htmlFor="workEnd"></label>
+								<input type="text" id="workEnd" placeholder="End Date" />
+							</li>
+							<Work work={this.state.work} />
+							<button type="button" onClick={this.addWork}>
+								Add Work
+							</button>
+						</ul>
+					</fieldset>
+					<fieldset>
+						<div className="school">
+							<legend>Education</legend>
+							<label htmlFor="school"></label>
+							<input type="text" id="school" placeholder="University" />
+							<label htmlFor="degree"></label>
+							<input type="text" id="degree" placeholder="Degree" />
+							<label htmlFor="schoolStart"></label>
+							<input type="text" id="study" placeholder="School of Study" />
+							<label htmlFor="study"></label>
+							<input type="text" id="schoolStart" placeholder="Start Date" />
+							<label htmlFor="schoolEnd"></label>
+							<input type="text" id="schoolEnd" placeholder="End Date" />
+							<button type="button">Add School</button>
+						</div>
+					</fieldset>
+					<button type="reset">Reset</button>
 				</form>
 			</div>
 		);
--- src/components/work.js
+++ src/components/work.js
@@ -3,22 +3,29 @@ import React from 'react';
 class Work extends React.Component {
 	constructor(props) {
 		super(props);
+		this.removeWork = this.removeWork.bind(this);
 	}
 
+	removeWork() {}
+
 	render() {
-		return (
-			<fieldset>
-				<legend>Work Experience</legend>
-				<label htmlFor="company"></label>
-				<input type="text" id="company" placeholder="Company" />
-				<label htmlFor="companyTitle"></label>
-				<input type="text" id="companyTitle" placeholder="Title" />
-				<label htmlFor="workStart"></label>
-				<input type="text" id="workStart" placeholder="Start Date" />
-				<label htmlFor="workEnd"></label>
-				<input type="text" id="workEnd" placeholder="End Date" />
-			</fieldset>
-		);
+		const { work } = this.props;
+		return work.map((item, index) => (
+			<li key={index}>
+				<div className="work">
+					<legend>Work Experience</legend>
+					<label htmlFor="company"></label>
+					<input type="text" id="company" placeholder="Company" />
+					<label htmlFor="companyTitle"></label>
+					<input type="text" id="companyTitle" placeholder="Title" />
+					<label htmlFor="workStart"></label>
+					<input type="text" id="workStart" placeholder="Start Date" />
+					<label htmlFor="workEnd"></label>
+					<input type="text" id="workEnd" placeholder="End Date" />
+					<button type="button">Delete Work</button>
+				</div>
+			</li>
+		));
 	}
 }
 
