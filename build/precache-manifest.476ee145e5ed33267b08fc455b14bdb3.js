c6ff15e387c80c7dd79c78ae25c835361e274586
ericchi00 <ericchi00@gmail.com>
2022-04-08 17:32:59 -0700

    adding in build

---
+++ build/precache-manifest.476ee145e5ed33267b08fc455b14bdb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a00b8efb0e694b9570e1",
+    "url": "/project-cv-application/static/css/main.783ef9a6.chunk.css"
+  },
+  {
+    "revision": "a00b8efb0e694b9570e1",
+    "url": "/project-cv-application/static/js/main.fa118045.chunk.js"
+  },
+  {
+    "revision": "1d74b4f77b9f21d14057",
+    "url": "/project-cv-application/static/js/runtime~main.e154a1c9.js"
+  },
+  {
+    "revision": "db2f2f34aa70b7a1ad55",
+    "url": "/project-cv-application/static/css/2.a8739b98.chunk.css"
+  },
+  {
+    "revision": "db2f2f34aa70b7a1ad55",
+    "url": "/project-cv-application/static/js/2.635a5868.chunk.js"
+  },
+  {
+    "revision": "30ebe1a21c7e3819527c456248d52768",
+    "url": "/project-cv-application/index.html"
+  }
+];
